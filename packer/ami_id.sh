773dda2550f6f9ef131504bf223b91178d9cccf4
MagenX <magenx@users.noreply.github.com>
2021-11-24 19:50:04 +0000

    Update ami_id.sh

--- packer/ami_id.sh
+++ packer/ami_id.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-AMI_ID=$(jq -r '.builds[-1].artifact_id' ./packer/manifest.json | cut -d ":" -f2)
+AMI_ID=$(jq -r '.builds[-1].artifact_id' ./packer/manifest_for_${INSTANCE_NAME}.json | cut -d ":" -f2)
 echo -n "{\"AMI_ID\":\"${AMI_ID}\"}"
